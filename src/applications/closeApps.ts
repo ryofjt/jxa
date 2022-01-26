bf73e3fac871ec7ed56daf23c81587e81eb7c83e
ryofjt <ryofjt12@gmail.com>
2022-01-27 01:17:02 +0900

    fix closeApps

--- src/applications/closeApps.ts
+++ src/applications/closeApps.ts
@@ -1,29 +1,35 @@
 import { run } from "@jxa/run";
 import { printStatement } from "../utils";
 
-export async function closeApps() {
-  return run((printStatement: string) => {
-    const print = eval(printStatement);
+export async function closeApps(terminalName = "ターミナル") {
+  return run(
+    (printStatement: string, terminalName: string) => {
+      const print = eval(printStatement);
 
-    const system = Application("System Events");
-    const dock = system.processes["Dock"];
+      const system = Application("System Events");
+      const dock = system.processes["Dock"];
 
-    // quit terminal
-    const terminal = Application("Terminal");
-    const windows = terminal.windows();
-    if (windows.length > 1) {
-      windows[1].frontmost = true;
-      system.keystroke("q", { using: "command down" });
-    }
+      // quit terminal
+      const terminal = Application("Terminal");
+      const windows = terminal.windows();
+      if (windows.length > 1) {
+        windows[1].frontmost = true;
+        system.keystroke("q", { using: "command down" });
+      }
 
-    const appNames: string[] = dock.lists[0]
-      .uiElements()
-      .map((v: any) => v.name());
-    appNames
-      .filter((v) => v !== "ターミナル")
-      .forEach((v: any) => {
-        Application(v).quit();
-        delay(0.1);
-      });
-  }, printStatement);
+      const appNames: string[] = dock.lists[0]
+        .uiElements()
+        .map((v: any) => v.name());
+      appNames
+        .filter((v) => v !== terminalName)
+        .forEach((v: any) => {
+          try {
+            Application(v).quit();
+          } catch (e) {}
+          delay(0.1);
+        });
+    },
+    printStatement,
+    terminalName
+  );
 }
